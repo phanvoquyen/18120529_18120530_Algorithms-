73649b13fcb5a439e979ebab4eda77eba3d53794
phanvoquyen <55779476+phanvoquyen@users.noreply.github.com>
2019-12-13 23:38:18 +0700

    update main

--- 18120529_18120530_Algorithms/18120529_18120530_Algorithms/main.cpp
+++ 18120529_18120530_Algorithms/18120529_18120530_Algorithms/main.cpp
@@ -1,27 +1,38 @@
 #include"HuffmanTree.h"
 #include"FileCompress.h"
+#include"compressFolder.h"
 #include<string>
 #include<iostream>
 int main()
 {
 	string filename;
-	cout << "Enter full your filename: ";
-	cin >> filename;
+
 	cout << "You want to:" << endl;
 	cout << "1. Compress" << endl;
 	cout << "2. UnCompress" << endl;
+	cout << "3: compress the foler" << endl;
+	cout << "4: decompress the folder" << endl;
 	cout << "0. Thoat" << endl;
-
+	cout << "Your choose: ";
 	int i = 0;
 	cin >> i;
+	cout << "Enter full your filename or folder: ";
+	cin >> filename;
 	switch (i)
 	{
 	case 1:
-		TestCompress(filename);
+		TestCompress(filename, filename);
 		break;
 	case 2:
-		TestUnCompress(filename);
+		TestUnCompress(filename, filename);
 		break;
+	case 3: {
+		DuyetDeNen(filename, filename);
+	}
+			break;
+	case 4: {
+		DuyetDeGiaiNen(filename, filename);
+	}
 	default:
 		break;
 	}
